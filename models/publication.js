0c19dfe2a8b4618b6a72d17300cdf5428813027b
Julia <juliapsrk@gmail.com>
2022-04-23 11:30:33 +0200

    first commit

---
+++ models/publication.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const publicationSchema = new mongoose.Schema(
+  {
+    message: {
+      type: String,
+      required: true,
+      maxlength: 300,
+      // trim: ensures that any spaces at the beginning or ending of string are removed
+      trim: true
+    },
+    picture: {
+      type: String
+    },
+    creator: {
+      // tell mongoose an ojbect id of another document
+      // will be stored in this property
+      type: mongoose.Types.ObjectId,
+      required: true,
+      // tell mongoose that this refers to the id
+      // of a document in the users scollection
+      // (the collection that corresponds to the "User" model)
+      ref: 'User'
+    }
+  },
+  { timestamps: true }
+);
+
+const Publication = mongoose.model('Publication', publicationSchema);
+
+module.exports = Publication;
---
+++ routes/meow.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const Publication = require('./../models/publication');
+const routeGuard = require('./../middleware/route-guard');
+const fileUpload = require('./../middleware/file-upload');
+
+const meowRouter = new express.Router();
+
+// GET - '/meow/create' - Renders meow creation page ✅
+meowRouter.get('/create', routeGuard, (req, res) => {
+  res.render('meow-create');
+});
+
+// POST - '/meow/create' - Handles new meow creation ✅
+meowRouter.post(
+  '/create',
+  routeGuard,
+  fileUpload.single('picture'),
+  (req, res, next) => {
+    const { message } = req.body;
+    // IF there is a picture,
+    // store the url in the picture variable
+    let picture;
+    if (req.file) {
+      picture = req.file.path;
+    }
+    // call create method on publication
+    Publication.create({
+      message,
+      picture,
+      creator: req.user._id
+    })
+      .then(() => {
+        res.redirect('/');
+      })
+      .catch((error) => {
+        next(error);
+      });
+  }
+);
+
+// GET - '/meow/:id' - Loads meow from database, renders single meow page ✅
+meowRouter.get('/:id', (req, res, next) => {
+  const { id } = req.params;
+  Publication.findById(id)
+    .populate('creator')
+    .then((publication) => {
+      res.render('meow-single', { publication });
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
+
+// GET - '/meow/:id/edit' - Loads meow from database, renders meow edit page ❌
+
+// POST - '/meow/:id/edit' - Handles edit form submission ❌
+
+// POST - '/meow/:id/delete' - Handles deletion ✅
+meowRouter.post('/:id/delete', (req, res, next) => {
+  const { id } = req.params;
+  Publication.findByIdAndRemove(id)
+    .then(() => {
+      res.redirect('/');
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
+
+module.exports = meowRouter;
